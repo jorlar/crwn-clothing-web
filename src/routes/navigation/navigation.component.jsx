8e3f33a9fd4ecebe2b06464d388aa3614f4595c5
jorlar <jqern.larsen@gmail.com>
2023-01-07 14:02:28 +0100

    :art: Add Signin page and routes

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -16,6 +16,10 @@ const Navigation = () => {
             <Link className="nav-link" to="/shop">
                 SHOP
             </Link>
+            <Link className="nav-link" to="/signIn">
+                Sign In
+            </Link>
+            
           </div>
         </div>
         <Outlet/>
