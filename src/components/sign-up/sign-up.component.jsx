99e2ad61d6840898701179a321f13716862c39a6
jorlar <jqern.larsen@gmail.com>
2023-01-09 14:16:08 +0100

    :art: Clean up and sentralize code

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,11 +1,10 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
-import { UserContext } from "../contexts/user.context";
 import '../sign-up/sign-up-form.styles.scss';
 
 const defaultFormFields = {
