008f8ac837b40c2b00397c2d9df3297e3c024e8a
jorlar <jqern.larsen@gmail.com>
2023-01-10 14:52:34 +0100

    :art: Create cart and functionallity

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,7 +2,10 @@ import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { UserContext } from "../../components/contexts/user.context";
+import { CartContext } from "../../components/contexts/cart.context";
 
 import {signOutUser} from '../../utils/firebase/firebase.utils'
 
@@ -10,6 +13,7 @@ import "./navigation.styles.scss";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const {isCartOpen} = useContext(CartContext)
 
   return (
     <Fragment>
@@ -30,7 +34,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+       {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
